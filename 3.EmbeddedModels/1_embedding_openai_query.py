ac02f0a190c47e0fd1d5c8801fc42d38b545fa3c
preethamgoud9 <preethamgoud229@gmail.com>
2025-04-23 17:50:14 +0530

    #embedding model

---
+++ 3.EmbeddedModels/1_embedding_openai_query.py
@@ -0,0 +1,10 @@
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large",dimensions=32)
+
+result = embeddings.embed_query("who is the inventor of AI")
+
+print(str(result))
