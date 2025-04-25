1da8bb6570559167020dcbe4a1e0df4cb73994e8
preethamgoud9 <preethamgoud229@gmail.com>
2025-04-26 00:54:03 +0530

    worked on StrOutPutParser in langchain

--- 2.ChatModels/with_structured_output_typeddict.py
+++ 2.ChatModels/with_structured_output_typeddict.py
@@ -1,17 +1,19 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-from typing import TypedDict,Annotated,Optional
-
+from typing import TypedDict,Annotated,Optional,Literal
+from pydantic import BaseModel , Field
 load_dotenv()
 
 model = ChatOpenAI()
 
-class Review(TypedDict):
-    key_themes : Annotated[list[str],'Write down all the key points in a list']
-    summary : Annotated[str,"A brief description of the review"]
-    sentiment: Annotated[str,'Return sentiment of the review either negative,positive or neutral']
-    pros:Annotated[Optional[list[str]],'Write down all the pros in a list']
-    cons:Annotated[Optional[list[str]],'Write down all the cons in a list']
+class Review(BaseModel):
+
+    key_themes: list[str] = Field(description='Write down all the key themes discussed in the review in a list') 
+    summary: str = Field(description="A brief description of the review")
+    sentiment: Literal['neg','neu','pos'] = Field(description="Return sentiment of the review either negative,positive or neutral'")
+    pros: Optional[list[str]] = Field(description='Write down all the pros in a list')
+    cons: Optional[list[str]] = Field(description="Write down all the cons in a list")
+                        
 
 Structured_model = model.with_structured_output(Review)
 
